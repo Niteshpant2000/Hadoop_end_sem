ee75a1187eec3db6c11f53cdd92a48194a67d123
NITESH_PANT <54784082+Niteshpant2000@users.noreply.github.com>
2021-06-17 10:22:12 +0530

    Create IndiaDriver.java

---
+++ IndiaDriver.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class IndiaDriver {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException 
+	{
+		Configuration conf = new Configuration();
+		
+		Job j = new Job();
+		j.setJobName("India");
+		j.setJarByClass(IndiaDriver.class );
+		j.setMapperClass(IndiaMapper.class );
+
+		j.setReducerClass(IndiaReducer.class);
+
+		
+		j.setOutputKeyClass(Text.class);
+		j.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(j, new Path(args[0]));
+		FileOutputFormat.setOutputPath(j, new Path(args[1]));
+		
+		URI uri = new URI(args[1].toString());
+		
+		FileSystem fs =  FileSystem.get(uri, conf);
+		
+		boolean x = fs.delete(new Path(uri),true);
+		
+		
+		int time =  j.waitForCompletion(true) ? 0 : 1;
+		
+	}
+
+}
